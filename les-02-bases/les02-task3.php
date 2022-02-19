8aabc9c25baecedd3ac6b38a9d1f4f5e44916f88
boeinfo <boeinfo@ya.ru>
2022-02-19 17:50:22 +0300

    The third task is ready

---
+++ les-02-bases/les02-task3.php
@@ -0,0 +1,23 @@
+<?php
+// -----------------------------------------------------
+// PHP Start | Theory
+// Lesson 2. https://php-start.com/lesson/php-start-theory/basics
+// 
+// Task 2. Variables naming.
+// -----------------------------------------------------
+
+/* 
+ * Student: Oleg Baranchikov (boeinfo@ya.ru)
+ * Date: 19.02.2022
+ * 
+ * https://www.php.net/manual/ru/userlandnaming.rules.php
+ * https://svyatoslav.biz/misc/psr_translation/#_PSR-1
+ */
+
+$channelName;           // название телеканала
+$manufacturerAddress;   // адрес производителя
+$autoColor;             // цвет автомобиля
+$waterTemperature;      // температура воды
+$telefonModel;          // модель телефона
+
+?>
