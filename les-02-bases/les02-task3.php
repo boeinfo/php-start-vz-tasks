1019d8f180125b9201687eac47026e684d855856
boeinfo <boeinfo@ya.ru>
2022-02-19 17:51:03 +0300

    The third task is ready

--- les-02-bases/les02-task3.php
+++ les-02-bases/les02-task3.php
@@ -3,21 +3,28 @@
 // PHP Start | Theory
 // Lesson 2. https://php-start.com/lesson/php-start-theory/basics
 // 
-// Task 2. Variables naming.
+// Task 3. Operators.
 // -----------------------------------------------------
 
 /* 
  * Student: Oleg Baranchikov (boeinfo@ya.ru)
  * Date: 19.02.2022
  * 
- * https://www.php.net/manual/ru/userlandnaming.rules.php
- * https://svyatoslav.biz/misc/psr_translation/#_PSR-1
+ * https://www.php.net/manual/ru/language.expressions.php
  */
 
-$channelName;           // название телеканала
-$manufacturerAddress;   // адрес производителя
-$autoColor;             // цвет автомобиля
-$waterTemperature;      // температура воды
-$telefonModel;          // модель телефона
+$numOne = 3;
+$numTwo = 5;
+$numThree = 8;
+
+echo "первое слагаемое: $numOne,\nвторое слагаемое: $numTwo,\n"
+        . "третье слагаемое: $numThree. \n";
+
+$result = $numOne + $numTwo +$numThree;
+
+echo nl2br("Сумма равна: $result. \n");
+
+$result = 2 + 6 + 2/5 - 1;
+echo "Результат вычисления: $result. \n";
 
 ?>
