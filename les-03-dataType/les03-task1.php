ccc59f2a627a04dfb5a673c00918809228791131
boeinfo <boeinfo@ya.ru>
2022-02-19 23:34:40 +0300

    Lesson 3 prepared

---
+++ les-03-dataType/les03-task1.php
@@ -0,0 +1,28 @@
+<?php
+/* -----------------------------------------------------
+ * 
+ * Course: PHP Start | Theory (by Victor Zinchenko)
+ * 
+ * Lesson 3. Data type. 
+ * https://php-start.com/lesson/php-start-theory/data-types
+ * 
+ * Task 1. Types of variables.
+ * 
+ * Student: Oleg Baranchikov (boeinfo@ya.ru)
+ * Date: 19.02.2022
+ * 
+ * Materials:
+ * https://www.php.net/manual/ru/language.variables.php
+ * 
+ * -----------------------------------------------------
+ */
+
+
+// Присваиваем переменным значения
+$a = 152;
+
+
+// Выводим информацию о типах
+echo "Тип переменной \$a со значением $a - " . gettype($a) . "<br>";
+
+?>
