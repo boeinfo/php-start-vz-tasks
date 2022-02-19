c8667282b9f3514945d2713673292bbfc861918f
boeinfo <boeinfo@ya.ru>
2022-02-20 00:24:12 +0300

    Task 1 from lesson 3 is ready.

--- les-03-dataType/les03-task1.php
+++ les-03-dataType/les03-task1.php
@@ -9,7 +9,7 @@
  * Task 1. Types of variables.
  * 
  * Student: Oleg Baranchikov (boeinfo@ya.ru)
- * Date: 19.02.2022
+ * Date: 20.02.2022
  * 
  * Materials:
  * https://www.php.net/manual/ru/language.variables.php
@@ -17,12 +17,21 @@
  * -----------------------------------------------------
  */
 
-
-// Присваиваем переменным значения
+// Variables definition
 $a = 152;
+$b = '152';
+$c = 'London';
+$d = array(152);
+$e = 15.2;
+$f = false;
+$g = true;
 
-
-// Выводим информацию о типах
-echo "Тип переменной \$a со значением $a - " . gettype($a) . "<br>";
-
-?>
+// Print variables type and value
+echo 'Переменные:<br>';
+echo '-$a имеет тип ' . gettype($a) . ' и значение ' . var_export($a, true) . '.<br>';
+echo '-$b имеет тип ' . gettype($b) . ' и значение ' . var_export($b, true) . '.<br>';
+echo '-$c имеет тип ' . gettype($c) . ' и значение ' . var_export($c, true) . '.<br>';
+echo '-$d имеет тип ' . gettype($d) . ' и значение ' . var_export($d, true) . '.<br>';
+echo '-$e имеет тип ' . gettype($e) . ' и значение ' . var_export($e, true) . '.<br>';
+echo '-$f имеет тип ' . gettype($f) . ' и значение ' . var_export($f, true) . '.<br>';
+echo '-$g имеет тип ' . gettype($g) . ' и значение ' . var_export($g, true) . '.<br>';
