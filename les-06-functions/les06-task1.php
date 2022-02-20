994efdca03564870b95dde4435b1ae06da46eedb
boeinfo <boeinfo@ya.ru>
2022-02-20 23:18:53 +0300

    Lesson 6 prepare.

--- les-06-functions/les06-task1.php
+++ les-06-functions/les06-task1.php
@@ -3,30 +3,16 @@
  * 
  * @course: PHP Start | Theory (by Victor Zinchenko)
  * 
- * @lesson 5. Cycles. 
- * @link: https://php-start.com/lesson/php-start-theory/cycles
+ * @lesson 6. Functions. 
+ * @link: https://php-start.com/lesson/php-start-theory/user-functions
  * 
- * @task 3. Site menu.
+ * @task 1. Information about products in the cart.
  * 
  * @author: Oleg Baranchikov (boeinfo@ya.ru)
- * @date: 20.02.2022
+ * @date: 21.02.2022
  * 
- * @link: https://www.php.net/manual/ru/control-structures.foreach.php
+ * @link: https://www.php.net/manual/ru/language.functions.php
  * 
  * -----------------------------------------------------
  */
 
-$button = array();
-
-for($i=10; $i>=1; $i--) {
-    $button[$i] = "Button {$i}";
-}
-// var_dump($button);
-natsort($button);
-//var_dump($button);
-
-echo "<ul>";
-foreach($button as $elem) {
-    echo "<li><a href=\"#\">".$elem."</a></li>";
-}
-echo "</ul>";
