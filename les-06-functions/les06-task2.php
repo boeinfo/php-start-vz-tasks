510a05e6b8c6c475b92898610e250780a7f189cf
boeinfo <boeinfo@ya.ru>
2022-02-22 23:04:41 +0300

    task 2 from lesson 6 is ready.

--- les-06-functions/les06-task2.php
+++ les-06-functions/les06-task2.php
@@ -6,7 +6,7 @@
  * @lesson 6. Functions. 
  * @link: https://php-start.com/lesson/php-start-theory/user-functions
  * 
- * @task 1. Information about products in the cart.
+ * @task 2. The quadratic equation.
  * 
  * @author: Oleg Baranchikov (boeinfo@ya.ru)
  * @date: 22.02.2022
@@ -16,31 +16,27 @@
  * -----------------------------------------------------
  */
 
-$products = [   [ "name" => "Телевизор", "price" => "400", "quantity" => 1 ],
-                [ "name" => "Телефон", "price" => "300", "quantity" => 3 ],
-                [ "name" => "Кроссовки", "price" => "150", "quantity" => 2 ]
-            ];
 
-$cart = [];
+// print_r(QuardEquation(1, 15, 56));  // -7, -8
+// print_r(QuardEquation(4, -4, 1));  // 0.5
+echo QuardEquation(5, 3, 7) ? "Ищи корни" : "Нет корней";  // Нет корней
 
-$cart = total_in_cart($products);
 
-echo "Общая сумма покупок: ".$cart["total_price"]."<br>";
-echo "Общее количество выбранных товаров: ".$cart["total_quantity"]."<br>";
-
-//functions block
-function total_in_cart($prod){
-
-    $total_price = 0;
-    $total_quantity = 0;
-
-    foreach ($prod as $val) {
-        //print_r($val["price"]." - ".$val["quantity"]."<br>");
-        $total_price += intval($val["price"]);
-        $total_quantity += intval($val["quantity"]);
+function QuardEquation($a, $b, $c) {
+    
+    $x1; $x2; $d;
+    
+    $d = pow($b, 2) - 4*$a*$c;
+    
+    if ($d < 0) {
+        return false;
+    } elseif ($d == 0) {
+        $x1 = -$b / (2 * $a);
+        return $x1;
+    } elseif ($d > 0) {
+        $x1 = round( (-$b + sqrt($d)) / (2 * $a) , 2);
+        $x2 = round( (-$b - sqrt($d)) / (2 * $a) , 2);
+        return [$x1, $x2];
     }
-
-    return ["total_price" => $total_price, 
-            "total_quantity" => $total_quantity];    
+        
 }
-
