4db66069a02fe2873c75d85b10aaff38849fd34e
boeinfo <boeinfo@ya.ru>
2022-02-22 22:41:43 +0300

    task 1 from lesson 6 is ready.

--- les-06-functions/les06-task1.php
+++ les-06-functions/les06-task1.php
@@ -9,10 +9,38 @@
  * @task 1. Information about products in the cart.
  * 
  * @author: Oleg Baranchikov (boeinfo@ya.ru)
- * @date: 21.02.2022
+ * @date: 22.02.2022
  * 
  * @link: https://www.php.net/manual/ru/language.functions.php
  * 
  * -----------------------------------------------------
  */
+
+$products = [   [ "name" => "Телевизор", "price" => "400", "quantity" => 1 ],
+                [ "name" => "Телефон", "price" => "300", "quantity" => 3 ],
+                [ "name" => "Кроссовки", "price" => "150", "quantity" => 2 ]
+            ];
+
+$cart = [];
+
+$cart = total_in_cart($products);
+
+echo "Общая сумма покупок: ".$cart["total_price"]."<br>";
+echo "Общее количество выбранных товаров: ".$cart["total_quantity"]."<br>";
+
+//functions block
+function total_in_cart($prod){
+
+    $total_price = 0;
+    $total_quantity = 0;
+
+    foreach ($prod as $val) {
+        //print_r($val["price"]." - ".$val["quantity"]."<br>");
+        $total_price += intval($val["price"]);
+        $total_quantity += intval($val["quantity"]);
+    }
+
+    return ["total_price" => $total_price, 
+            "total_quantity" => $total_quantity];    
+}
 
