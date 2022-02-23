69bbb5eb822dcf25f61a849d6dbc2b7b2f55067c
boeinfo <boeinfo@ya.ru>
2022-02-23 21:38:39 +0300

    lesson 9 prepare.

---
+++ les-09-cookie/les09-tsk01.php
@@ -0,0 +1,18 @@
+<?php
+/* -----------------------------------------------------
+ * 
+ * @course: PHP Start | Theory (by Victor Zinchenko)
+ * 
+ * @lesson 9. Cookie. 
+ * @link: https://php-start.com/lesson/php-start-theory/cookie
+ * 
+ * @task 1. Cookie lifetime.
+ * 
+ * @author: Oleg Baranchikov (boeinfo@ya.ru)
+ * @date: 24.02.2022
+ * 
+ * @link: https://www.php.net/manual/ru/function.setcookie.php
+ * 
+ * -----------------------------------------------------
+ */
+
