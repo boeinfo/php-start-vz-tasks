011d71e77ce5860b94579b2499f0ee230afcb3c5
boeinfo <boeinfo@ya.ru>
2022-02-23 16:36:39 +0300

    Lesson 8 prepare.

---
+++ les-08-http_forms/les-08-task01.php
@@ -0,0 +1,17 @@
+<?php
+/* -----------------------------------------------------
+ * 
+ * @course: PHP Start | Theory (by Victor Zinchenko)
+ * 
+ * @lesson 8. Protocol HTTP and forms. 
+ * @link: https://php-start.com/lesson/php-start-theory/http-forms
+ * 
+ * @task 1. Request & Response
+ * 
+ * @author: Oleg Baranchikov (boeinfo@ya.ru)
+ * @date: 23.02.2022
+ * 
+ * @link: 
+ * 
+ * -----------------------------------------------------
+ */
---
+++ les-08-http_forms/les-08-task2.php
@@ -0,0 +1,19 @@
+<?php
+/* -----------------------------------------------------
+ * 
+ * @course: PHP Start | Theory (by Victor Zinchenko)
+ * 
+ * @lesson 8. Protocol HTTP and forms. 
+ * @link: https://php-start.com/lesson/php-start-theory/http-forms
+ * 
+ * @task 2. Forms using.
+ * 
+ * @author: Oleg Baranchikov (boeinfo@ya.ru)
+ * @date: 23.02.2022
+ * 
+ * @link: 
+ * 
+ * -----------------------------------------------------
+ */
+
+
