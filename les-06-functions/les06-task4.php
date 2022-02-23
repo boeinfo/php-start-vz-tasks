8312559d8e2f9cf8b246ef8ef9f7642545858180
boeinfo <boeinfo@ya.ru>
2022-02-23 11:41:29 +0300

    task 4 from lesson 6 prepare.

---
+++ les-06-functions/les06-task4.php
@@ -0,0 +1,38 @@
+/* -----------------------------------------------------
+ * 
+ * @course: PHP Start | Theory (by Victor Zinchenko)
+ * 
+ * @lesson 6. Functions. 
+ * @link: https://php-start.com/lesson/php-start-theory/user-functions
+ * 
+ * @task 4. Removing negative elements from the array (option 2).
+ * 
+ * @author: Oleg Baranchikov (boeinfo@ya.ru)
+ * @date: 23.02.2022
+ * 
+ * @link: https://www.php.net/manual/ru/language.references.whatdo.php
+ * @link: https://www.php.net/manual/ru/functions.arguments.php
+ * 
+ * -----------------------------------------------------
+ */
+
+$digits = [2, 10, -2, 4, 5, -1, 6, 200, 1.6, 95];
+var_dump($digits, true);
+
+$d = $digits;
+degNeg(&$d);
+var_dump($d);
+
+// function block
+function degNeg(&$dig = null) {
+    if(is_array($dig)) {
+        foreach($dig as $key => $val) {
+            if($val <= 0) {
+                unset($dig[$key]);
+            }
+        }
+        var_dump($dig);
+    } else {
+        echo 'Ошибка: Передан не массив.';
+    }    
+}
