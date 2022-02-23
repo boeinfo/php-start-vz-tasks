0a2ca5332c61b7369ee0858d894254468caf0c46
boeinfo <boeinfo@ya.ru>
2022-02-23 21:33:47 +0300

    task 2 lesson 8 passed.

--- les-08-http_forms/les-08-task2.php
+++ les-08-http_forms/les-08-task2.php
@@ -16,4 +16,47 @@
  * -----------------------------------------------------
  */
 
+if (isset($_POST['submit'])) {
+    
+    $p = "";
+    $un = isset($_POST['user_name']) ? $_POST['user_name'] : "";
+    $us = isset($_POST['user_surname']) ? $_POST['user_surname'] : "";;
+    $g = isset($_POST['gender']) ? $_POST['gender'] : "";
 
+    if ($g == "male") {
+        $p = "мистер";
+    } elseif ($g == "female") {
+        $p = "мисcис";
+    }
+    
+    echo "Добро пожаловать, {$p} {$us} {$un} !";
+} 
+
+?>
+<head>
+    <title>t2 l8</title>
+    <meta charset = "utf-8">
+</head>
+<body>
+    <h2>Давайте знакомится</h2>
+    <form action="les-08-task2.php" method="post">
+        <fieldset>
+            <legend>Ваше имя</legend>
+            <label for="name">Имя</label>
+            <input type="text" id="name" name="user_name"><br>
+            <label for="surname">Фамилия</label>
+            <input type="text" id="surname" name="user_surname"><br>
+        </fieldset>
+        <fieldset>
+            <legend>Кто Вы?</legend>
+            <label><input type="radio" name="gender" value="male">муж.</label>
+            <label><input type="radio" name="gender" value="female">жен.</label>
+        </fieldset>
+        <input type="submit" name="submit" value="submit">
+   </form>
+   <hr>
+</body>
+
+<?php
+
+?>
