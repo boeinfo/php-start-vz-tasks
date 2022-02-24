915ed347e95b856961dc804cc567c6048eca1e4a
boeinfo <boeinfo@ya.ru>
2022-02-24 22:53:46 +0300

    task 1 from lesson 9 is ready.

--- les-09-cookie/les09-tsk01.php
+++ les-09-cookie/les09-tsk01.php
@@ -16,3 +16,41 @@
  * -----------------------------------------------------
  */
 
+const NOVICE = "Добро пожаловать, новичек!";
+const FRIEND = "С возвращением, дружище!"; 
+const COOKIE_NAME = "IS_FRIEND";
+const COOKIE_TIME = 60*60*10;
+
+$greeting = "";
+
+if(isset($_COOKIE[COOKIE_NAME])){
+    //кука установлена
+    $greeting = FRIEND;
+}
+else {
+    //кука не установлена
+    setcookie(COOKIE_NAME, true, time() + COOKIE_TIME);
+    $greeting = NOVICE;
+}
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>l8-t1 cookie</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="container">
+            <header><h1>Приветствуем!</h1></header>
+            <main>
+                <p><?php 
+                    echo $greeting;
+                ?></p>
+            </main>
+            <footer>
+                <span>We are waiting for you at: </span><!--
+                --><em>Brest, av. Masherova</em><br>
+            </footer>
+        </div>
+    </body>
+</html>
