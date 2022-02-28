84783ad0fc483ab2febde32ef21608c914ef4429
boeinfo <boeinfo@ya.ru>
2022-02-28 19:49:38 +0300

    task 1 lesson 11 (db1) prepared.

---
+++ les-11-db1/news_site-insertData.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$sql = "INSERT INTO `news_category` "
+        . "(`id`, `name`, `description`, `sort_order`, `status`) VALUES "
+        . "(NULL, \'ф\', \'ффф\', \'15\', \'1\');";
+
+?>
+
