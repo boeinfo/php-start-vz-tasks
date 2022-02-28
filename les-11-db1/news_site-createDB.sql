125aaf040cbce92d01c6f8e5e3cdf169f4876754
boeinfo <boeinfo@ya.ru>
2022-03-01 00:13:23 +0300

    task 1 lesson 11 is ready. Lesson 11 passed.

--- les-11-db1/news_site-createDB.sql
+++ les-11-db1/news_site-createDB.sql
@@ -6,7 +6,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+03:00";
 
 CREATE DATABASE IF NOT EXISTS `news_site` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 COMMIT;
--- les-11-db1/news_site-insertData.php
+++ les-11-db1/news_site-insertData.php
@@ -1,9 +1,21 @@
 <?php
 
+## Форма записи запроса
 
 $sql = "INSERT INTO `news_category` "
         . "(`id`, `name`, `description`, `sort_order`, `status`) VALUES "
         . "(NULL, \'ф\', \'ффф\', \'15\', \'1\');";
+
+
+
+$sql = "INSERT INTO `user` "
+        . "(`name`, `surname`, `email`, `password`, `country`, `city`, "
+        . "`gender`, `birthdate`, `register_date`, `last_visit`) "
+        . "VALUES (\'Oleg\', \'Baranchikov\', \'boeinfo@ya.ru\', \'loop\', "
+        . "\'Belarus\', \'Brest\', \'1\', \'1975-01-22 14:25:00.000000\', "
+        . "CURRENT_TIMESTAMP, \'2022-02-01 17:36:52.000000\');";
+
+
 
 ?>
 
