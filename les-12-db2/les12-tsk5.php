dd0d6de7867e79ebb18afcd1b1ee85a5dcd1fb7c
boeinfo <boeinfo@ya.ru>
2022-03-01 21:26:30 +0300

    task 5 from lesson 12 is ready. Lesson 12 passed.

--- les-12-db2/les12-tsk5.php
+++ les-12-db2/les12-tsk5.php
@@ -3,7 +3,7 @@
  * 
  * @course: PHP Start | Theory (by Victor Zinchenko)
  * 
- * @lesson 11. DB. 
+ * @lesson 12. DB. 
  * @link: https://php-start.com/lesson/php-start-theory/databases-part-1
  * 
  * @task 5. SELECT statement + PHP.
@@ -22,44 +22,34 @@ if(mysqli_connect_errno()) {
     echo "Ошибка подключения к БД: ".mysqli_connect_error();
 } 
 else {
-    /*
-    $sql = "SELECT * FROM news;";
+    $sql = "SELECT n.h1, n.short_content, n.id ".
+                "FROM news AS n WHERE n.status = '1';";
     $result = mysqli_query($con, $sql);
 
-    if(isset($result)) {
-        echo "<pre>";
-        var_dump($result);
-        echo "</pre>";
-        echo "<hr>";
-        
-        $count = mysqli_num_rows($result);
-        
-        if($count) {
-            while($row = mysqli_fetch_array($result)) {
-                echo "<h4>".$row['h1']."</h4>";
-                echo ">> ".$row['category_id'].", ".$row['author_id'].
-                        ", ".$row['date']."<br>";
-                echo $row['short_content']."<br>";                
-            }
-        }
-        
-        echo "<hr>";
+    if(!$result) {
+        echo "Не получается получить данные".mysqli_error($con);
+    }    
+    /*
+    foreach ($result as $val) {
+        echo "<h4>".$val['h1']."</h4>";
+        echo $val['short_content']."<br>";   
     }
     */
 }
 ?>
-<!--
 <!DOCTYPE HTML>
-<html>
+<html lang="ru">
     <head>
-        <title></title>
+        <title>les12-t5</title>
+        <meta charset="utf-8">
     </head>
     <body>
         <?php
-        /*
-         * 
-         */
+             foreach ($result as $val) {
+                echo "<h1>".$val['h1']."</h1>";
+                echo "<p>".$val['short_content']."</p>";
+                echo "<a href=\"/news/view/{$val['id']}\">Читать далее...</a>";
+            }
         ?>
     </body>
 </html>
--->
--- les-12-db2/les12-tsk5.sql
+++ les-12-db2/les12-tsk5.sql
@@ -3,7 +3,6 @@
  * Created: 1 мар. 2022 г.
  */
 
-USE `news_site`;
+USE news_site;
 
-
-
+SELECT n.h1, n.short_content, n.id FROM news AS n WHERE n.status = '1';
