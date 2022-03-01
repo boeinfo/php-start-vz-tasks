53832e8b2b0296fc4d731636be6c95398529ebd6
boeinfo <boeinfo@ya.ru>
2022-03-01 19:05:36 +0300

    task 1 from lesson 11 is ready.

--- les-12-db2/les12-tsk1.sql
+++ les-12-db2/les12-tsk1.sql
@@ -16,3 +16,21 @@ VALUES ('Alex', 'Prost', 'prost@gmail.com', 'plumBum', 'USA', 'Hollywood',
        ('Siri', 'Goolee', 'siri@pisem.net', 'parol', 'India', 'New Diles', 
 '0', '2001-03-05 08:51:00.000000', CURRENT_TIMESTAMP);
 
+
+/*
+* Data for second example
+*/
+-- INSERT INTO `user` (`name`, `surname`, `email`, `password`, `country`, `city`, 
+--                     `gender`, `birthdate`, `register_date`) 
+-- VALUES ('Tomas', 'Prost', 'prost@gmail.com', 'plumBum', 'UFO', 'Hollywood', 
+-- '1', '1982-11-02 23:47:00.000000', CURRENT_TIMESTAMP),
+--        ('John', 'Smith', 'e.wice@protonmail.blr', 'moonlight', 'UFO', 'Yan', 
+-- '0', '1992-10-30 04:22:00.000000', CURRENT_TIMESTAMP),
+--        ('Siri', 'Smith', 'siri@pisem.net', 'parol', 'UFO', 'New Diles', 
+-- '0', '2001-03-05 08:51:00.000000', CURRENT_TIMESTAMP),
+--        ('Tomas', 'Duke', 'e.wice@protonmail.blr', 'moonlight', 'UFO', 'Yan', 
+-- '0', '1992-10-30 04:22:00.000000', CURRENT_TIMESTAMP),
+--        ('Tim', 'Benz', 'e.wice@protonmail.blr', 'moonlight', 'UFO', 'Yan', 
+-- '0', '1992-10-30 04:22:00.000000', CURRENT_TIMESTAMP);
+
+
