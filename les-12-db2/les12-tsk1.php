3e71a48df3566afd6fd5aae80b34ce99080a2b77
boeinfo <boeinfo@ya.ru>
2022-03-01 19:12:05 +0300

    task 1 from lesson 11 on php is ready.

--- les-12-db2/les12-tsk1.php
+++ les-12-db2/les12-tsk1.php
@@ -22,44 +22,22 @@ if(mysqli_connect_errno()) {
     echo "Ошибка подключения к БД: ".mysqli_connect_error();
 } 
 else {
-    /*
-    $sql = "SELECT * FROM news;";
+    $sql = "INSERT INTO `user` (`name`, `surname`, `email`, `password`, 
+        `country`, `city`, `gender`, `birthdate`, `register_date`) 
+        VALUES ('Alex', 'Prost', 'prost@gmail.com', 'plumBum', 'USA', 
+        'Hollywood', '1', '1982-11-02 23:47:00.000000', CURRENT_TIMESTAMP),
+       ('Elen', 'Wice', 'e.wice@protonmail.blr', 'moonlight', 'Belarus', 'Yan', 
+        '0', '1992-10-30 04:22:00.000000', CURRENT_TIMESTAMP),
+       ('Siri', 'Goolee', 'siri@pisem.net', 'parol', 'India', 'New Diles', 
+        '0', '2001-03-05 08:51:00.000000', CURRENT_TIMESTAMP);";
+    
     $result = mysqli_query($con, $sql);
 
-    if(isset($result)) {
-        echo "<pre>";
-        var_dump($result);
-        echo "</pre>";
-        echo "<hr>";
-        
-        $count = mysqli_num_rows($result);
-        
-        if($count) {
-            while($row = mysqli_fetch_array($result)) {
-                echo "<h4>".$row['h1']."</h4>";
-                echo ">> ".$row['category_id'].", ".$row['author_id'].
-                        ", ".$row['date']."<br>";
-                echo $row['short_content']."<br>";                
-            }
-        }
-        
-        echo "<hr>";
-    }
-    */
+//    if(isset($result)) {
+//        echo "<pre>";
+//        var_dump($result);
+//        echo "</pre>";
+//        echo "<hr>";
+//    }    
 }
 ?>
-<!--
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <?php
-        /*
-         * 
-         */
-        ?>
-    </body>
-</html>
--->
